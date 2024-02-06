bf1bf3f9e64f079b453c91849e76646ec9f0772f
Azman Sabbir <asabbir@transportedlabs.com>
2024-02-07 01:59:11 +0600

    demo data

--- tests/QuickStart/ID00-ProjectSetup.test.ts
+++ tests/QuickStart/ID00-ProjectSetup.test.ts
@@ -76,22 +76,86 @@ test.describe('Ideeza Full Project Setup', () => {
 
                 // const ele = await page.locator("//span[text()='80001']")
                 // await expect(ele).toBeVisible()
+
+
+                await page.goto("/user/dashboard/code/add-part", { timeout: 1200000, waitUntil: "domcontentloaded" })
+
+
+
+                const newProjectPages = new newProjectPage(pages[1])
+
+
+
+                await newProjectPages.clickTakeATourStartBtn()
+                await newProjectPages.clickTakeATourSkipBtn()
+
+                await test.step("Part Create With Code | Add Part Section", async () => {
+
+                        await newProjectPages.inputCodeBlockName()
+                        await newProjectPages.clickOnColorSection()
+                        await newProjectPages.clickOnCatagorySection()
+                        await newProjectPages.clickToSelectCodeCatagory()
+                        await newProjectPages.clickOnPartNextStepBtn()
+
+                })
+
+                await test.step("Add New Part With CodePart Create With Code | Block Parameters Section", async () => {
+
+                        await newProjectPages.checkValueInput()
+                        await newProjectPages.inputVariableName()
+                        await newProjectPages.checkPreviousConnector()
+                        await newProjectPages.checkNextConnector()
+                        await newProjectPages.checkIsOutput()
+                        await newProjectPages.checkInputInline()
+                        await newProjectPages.checkStatementInput()
+                        await newProjectPages.checkAppendValueInput()
+                        await newProjectPages.input2VariableName()
+                        await newProjectPages.clickOnPartNextStepBtn()
+
+                })
+
+                await test.step("Add New Part With CodePart Create With Code | General Section", async () => {
+
+                        await newProjectPages.inputCodePartName()
+                        await newProjectPages.inputCodePartDescription()
+                        await newProjectPages.clickOnCatagorySection()
+                        await newProjectPages.clickToSelectCodeCatagory()
+                        await newProjectPages.clickOnShowOnCustomizeCheckBox()
+                        await newProjectPages.clickOnGeneralSectionNextStepBtn()
+
+                })
+
+
+                //Choose Block Chain Mint
+                await newProjectPages.clickToChooseBlockChainMint("Mumbai Testnet (Polygon)")
+                //Click on Choose Collection section
+                await newProjectPages.clickToChooseProjectCollections()
+                //select collection
+                await newProjectPages.clickToChooseProjectCollection("1")
+
+                //input Part Privet Price
+                await newProjectPages.inputPartPrivetPrice()
+
+                //input Commarcial Price
+                await newProjectPages.inputCommarcialPrice()
+                //click on the confirm checkbox
+                await newProjectPages.clickOnTheConfirmCheckBox()
+                //click on the next step button
+                await newProjectPages.clickOnGeneralSectionNextStepBtn()
+
+
+                await newProjectPages.clickOnConnectWalletBtn()
+                //Click On metamask button
+                // let newOne = null;
+                await newProjectPages.clickOnMetaMaskBtn()
+                await newProjectPages.approveMetaMask()
+
+                await newProjectPages.clickOnContinueButton()
+                await page.waitForLoadState("load")
+
+                //Click On Agree Btn
+                await newProjectPages.clickOnAGreeBtn()
+                await page.waitForLoadState("load")
         });
-
-        test.only('demo', async ({ page }) => {
-                // console.log(Math.floor(Math.random() * 10) + 1);
-                // console.log(Math.floor(Math.random() * (1 - 10 + 1)) + 11);
-                // const sequence = [...Array(1).keys()]
-                // console.log(sequence)
-
-                // console.log(Math.floor(Math.random() * (2 - 1 + 1)) + 5);
-
-                console.log(Math.floor(Math.random() * 10) + 1);
-
-
-        })
-
-
-
 
 })
