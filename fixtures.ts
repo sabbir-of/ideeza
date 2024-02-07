31beac6bf991678dbe2127f55dabdd0daf3435b5
Azman Sabbir <asabbir@transportedlabs.com>
2024-02-07 20:53:10 +0600

    updated something

--- fixtures.ts
+++ fixtures.ts
@@ -1,51 +1,37 @@
-import { test as base, chromium, type BrowserContext } from '@playwright/test';
-import metaMaskPage from '@pages/metamask.page';
-const path = require('path');
-const userDirData = path.join(__dirname, '/tests/QuickStart/User-Data-Dir/Chrome/User Data/Default/Extensions/nkbihfbeogaeaoehlefnkodbefgpgknn/10.35.1_0');
+import { BrowserContext, expect, test as baseTest } from "@playwright/test";
+import dappwright, { Dappwright, MetaMaskWallet } from "@tenkeylabs/dappwright";
 
-export const test = base.extend<{
+export const test = baseTest.extend<{
   context: BrowserContext;
-  extensionId: string;
+  wallet: Dappwright;
 }>({
-  context: async ({ }, use) => {
-    const pathToExtension = path.join(__dirname, '/MyMetamaskExtension');
-    const context = await chromium.launchPersistentContext(userDirData, {
+  context: async ({}, use) => {
+    // Launch context with extension
+    const [wallet, _, context] = await dappwright.bootstrap("", {
+      wallet: "metamask",
+      version: MetaMaskWallet.recommendedVersion,
+      seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
       headless: false,
-      args: [
-        `--disable-extensions-except=${pathToExtension}`,
-        `--load-extension=${pathToExtension}`,
-      ],
     });
+
+    // Add Hardhat as a custom network
+    await wallet.addNetwork({
+      networkName: "Polygon Testnet",
+      rpc: "https://polygon-mumbai.infura.io/v3/4ca16cf92c974dde8f9487c7d32b953a",
+      chainId: 80001,
+      symbol: "MATIC",
+    });
+
+    await wallet.importPK('17a75569989837200a68b0e0a41a19d0bd9996104011f098fecf912b6fcd8c20');
 
 
 
     await use(context);
-    await context.close();
-
-
-
   },
-  extensionId: async ({ context }, use) => {
-    /*
-    // for manifest v2:
-    let [background] = context.backgroundPages()
-    if (!background)
-      background = await context.waitForEvent('backgroundpage')
-    */
 
-    // for manifest v3:
-    let [background] = context.serviceWorkers();
-    if (!background)
-      background = await context.waitForEvent('serviceworker');
+  wallet: async ({ context }, use) => {
+    const metamask = await dappwright.getWallet("metamask", context);
 
-    const extensionId = background.url().split('/')[2];
-    await use(extensionId);
-
-
+    await use(metamask);
   },
 });
-export const expect = test.expect;
-
-export { chromium };
-
-
