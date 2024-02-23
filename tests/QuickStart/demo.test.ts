8f805defb77e411cd24b6d46771501b20dc19e68
Azman Sabbir <asabbir@transportedlabs.com>
2024-02-23 16:45:11 +0600

    dd

--- tests/QuickStart/demo.test.ts
+++
@@ -1,7 +0,0 @@
-import test from "playwright/test";
-
-const getNumber = Math.floor(Math.random() * 10)
-test.only('', async ({page }) => {
-        await await page.goto('/');
-
-})
--- utils/env.ts
+++
@@ -1,9 +0,0 @@
-export default class ENV {
-        public static BASE_URL = process.env.BASE_URL
-        public static USERNAME = process.env.USERNAME
-        public static PASSWORD = process.env.PASSWORD
-        public static ADMINEMAIL = process.env.ADMINEMAIL
-        public static ADMINPASSWORD = process.env.ADMINPASSWORD
-        public static TOKEN = process.env.TOKEN
-
-}
